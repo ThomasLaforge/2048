cec00a481103b54cb815324de389cec969101ec1
Thomas Laforge <thomas.laforge.38@gmail.com>
2016-11-10 18:34:31 +0100

    Init and debugginf model

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+window.jQuery = window.$ = require('jquery');
+import { Grid } from './Grid';
+import { KeycodeMap } from './Enums';
+
+let grid = new Grid();
+
+function getKeyboard(keyCode){
+    return Object.keys(KeycodeMap).filter(function(key) {return KeycodeMap[key] === keyCode})[0];
+}
+
+$('body').keyup( (e) => {
+    grid.action(e.keyCode);
+});
