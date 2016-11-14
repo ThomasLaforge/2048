93856d925dcf9e1d2c435ce692f04855a9b26354
Thomas Laforge <thomas.laforge.38@gmail.com>
2016-11-14 14:18:00 +0100

    score

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,10 @@ let grid = new Grid();
 function getKeyboard(keyCode){
     return Object.keys(KeycodeMap).filter(function(key) {return KeycodeMap[key] === keyCode})[0];
 }
+let score = 0;
 
 $('body').keyup( (e) => {
     grid.action(e.keyCode);
+        score += grid.action(e.keyCode);
+        $('#score').html(score);
 });
