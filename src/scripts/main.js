ca7440f97103aec0dd773b5516d7edd5d345c022
Thomas Laforge <thomas.laforge.38@gmail.com>
2016-11-14 14:23:08 +0100

    remove unused function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,6 @@ import { Grid } from './Grid';
 import { KeycodeMap } from './Enums';
 
 let grid = new Grid();
-
-function getKeyboard(keyCode){
-    return Object.keys(KeycodeMap).filter(function(key) {return KeycodeMap[key] === keyCode})[0];
-}
 let score = 0;
 
 $('body').keyup( (e) => {
